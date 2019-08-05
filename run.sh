7206f205710a372849a3da5132ea10fe75b02102
Alexis Rodríguez <alexis.rodriguez@laagenciadigital.com>
2019-08-05 09:16:48 -0500

    Added azure uplaod capabilities

--- run.sh
+++ run.sh
@@ -7,6 +7,11 @@ MONGODB_PORT=${MONGODB_PORT_1_27017_TCP_PORT:-${MONGODB_PORT}}
 MONGODB_USER=${MONGODB_USER:-${MONGODB_ENV_MONGODB_USER}}
 MONGODB_PASS=${MONGODB_PASS:-${MONGODB_ENV_MONGODB_PASS}}
 
+if [ -z "$DB_DUMP_TARGET" ]; then
+  echo "DB_DUMP_TARGET variable is required. Exiting."
+  exit 1
+fi
+
 [[ ( -z "${MONGODB_USER}" ) && ( -n "${MONGODB_PASS}" ) ]] && MONGODB_USER='admin'
 
 [[ ( -n "${MONGODB_USER}" ) ]] && USER_STR=" --username ${MONGODB_USER}"
@@ -19,16 +24,23 @@ echo "=> Creating backup script"
 rm -f /backup.sh
 cat <<EOF >> /backup.sh
 #!/bin/bash
+
+. /functions.sh
+
 MAX_BACKUPS=${MAX_BACKUPS}
-BACKUP_NAME=\$(date +\%Y.\%m.\%d.\%H\%M\%S)
+BACKUP_NAME=mongodb-\$(date +\%Y.\%m.\%d.\%H\%M\%S)
+echo "Backup name set as \${BACKUP_NAME}"
 
 echo "=> Backup started"
 if ${BACKUP_CMD} ;then
     echo "   Backup succeeded"
+    tar cfz "/backup/\${BACKUP_NAME}.tar.gz" "/backup/\${BACKUP_NAME}"
+    azure_upload "/backup/\${BACKUP_NAME}.tar.gz" "\${BACKUP_NAME}.tar.gz" \${AZ_ACCOUNT} \${AZ_CONTAINER} \${AZURE_SAS}
 else
     echo "   Backup failed"
-    rm -rf /backup/\${BACKUP_NAME}
 fi
+
+rm -rf /backup/\${BACKUP_NAME}
 
 if [ -n "\${MAX_BACKUPS}" ]; then
     while [ \$(ls /backup -N1 | wc -l) -gt \${MAX_BACKUPS} ];
